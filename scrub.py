09ba593da925fa03b8a733ad467911f46b1bf2e6
Paul Smith <smith.paul.anthony@gmail.com>
2014-07-21 21:10:01 -0700

    New file scrub.py added. Similar to correctCSV.py but slightly different in the details.
    Extended the workbook TwoSents.ipynb.

---
+++ scrub.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import string
+import argparse
+
+#Similar to correctCSV.py
+
+def simple_scrub(fin, fout):
+  f = open(fin, 'r')
+  g = open(fout, 'w')
+
+  for line in f:
+    filter(lambda x: x in string.printable, line)
+    g.write(line)
+  f.close()
+  g.close()
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Scrubs a .csv file of non-ASCII characters')
+  parser.add_argument('csvin')
+  parser.add_argument('csvout')
+  args = parser.parse_args()
+  
+  simple_scrub(args.csvin, args.csvout)
+
+if __name__ == '__main__':
+  main()
